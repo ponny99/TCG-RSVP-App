3461b34211dcce1ee09f3eda96e07f05edd14a12
Selfmade20e <tebogo1686@gmail.com>
2020-11-06 11:29:54 +0200

    adding all the changes I made on the app and fixed the errors on the app

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ function save() {
   info.fname = document.getElementById("fname").value;
   info.lname = document.getElementById("lname").value;
   info.address = document.getElementById("address").value;
+  info.dietary = document.getElementById("dietary").value;
   info.phone = document.getElementById("phone").value;
 
   if (
     info.fname === "" ||
     info.lname === "" ||
     info.address === "" ||
+    info.dietary === "" ||
     info.phone === ""
   ) {
     alert("Fields must be fill out first");
@@ -25,12 +27,13 @@ function save() {
   var fname = document.getElementById("fname").value;
   var lname = document.getElementById("lname").value;
   var address = document.getElementById("address").value;
+  var dietary = document.getElementById("dietary").value;
   var phone = document.getElementById("phone").value;
-
   
   localStorage.setItem("First Name", fname);
   localStorage.setItem("Last Name", lname);
   localStorage.setItem("Address", address);
+  localStorage.setItem("Dietary", dietary);
   localStorage.setItem("Phone Number", phone);
 
   
@@ -42,6 +45,7 @@ function deleteElement() {
   localStorage.removeItem("First Name");
   localStorage.removeItem("Last Name");
   localStorage.removeItem("Address");
+  localStorage.removeItem("Dietary Requirements");
   localStorage.removeItem("Phone Number");
   localStorage.removeItem("Info");
   readAll();
@@ -63,6 +67,7 @@ function readAll() {
                  First Name: ${formLocalStorage.fname}<br> 
                  Last Name: ${formLocalStorage.lname}<br>
                  Address: ${formLocalStorage.address}<br>
+                 Dietary Requirements:  ${formLocalStorage.dietary}<br>
                  Phone Number: ${formLocalStorage.phone}`;
   }
   document.getElementById("display-data").removeAttribute("hidden");
